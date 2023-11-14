0390be89bc3f8b91caadc4a55fe7cf2ca6350a1d
cieloazul310 <cieloazul310@gmail.com>
2023-11-14 16:02:21 +0900

    chore: add astro

--- packages/canvasmap/src/tilemap/vectorLayers.ts
+++ packages/canvasmap/src/tilemap/vectorLayers.ts
@@ -58,7 +58,7 @@ export const road: VectorLayer = {
 function contourWidth({ properties }: Feature): number {
   if (!properties || !("alti" in properties)) return 0;
   const { alti } = properties;
-  return (alti as number) % 10 === 0 ? 2 : 1;
+  return (alti as number) % 50 === 0 ? 1 : 0.5;
 }
 
 export const contour: VectorLayer = {
