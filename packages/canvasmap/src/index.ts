f834b13130045f8c7d8cff42fb103eecdb9d8dce
cieloazul310 <cieloazul310@gmail.com>
2023-11-20 22:19:46 +0900

    chore: fix

--- packages/canvasmap/src/index.ts
+++ packages/canvasmap/src/index.ts
@@ -1,6 +1,3 @@
-export * from "@cieloazul310/canvasmap-styles";
-export * from "@cieloazul310/canvasmap-utils";
-
 export { default as CanvasMap, type CanvasMapOptions } from "./CanvasMap";
 export {
   default as CanvasMapBrowser,
